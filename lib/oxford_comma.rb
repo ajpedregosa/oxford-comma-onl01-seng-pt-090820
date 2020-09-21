418be836385ed570b53b4f42c0816b93f5cee061
Jay Pedregosa <alexanderj.pedregosa@gmail.com>
2020-09-21 15:57:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,17 +3,4 @@ def oxford_comma(array)
     return "#{array[0]}"
   elsif array.length == 2
     return array.join(" and ")
-  elseif a
-end
-
-
-def oxford_comma(array)
-  if array.length == 1
-    return "#{array[0]}"
-  elsif array.length == 2
-    return array.join(" and ")
-  elsif array.length >= 3
-    array[-1] = "and #{array[-1]}"
-    return array.join(", ")
-  end
 end
